a8d07a14ba561cdaa81a1b887c3da2c194c4e258
Slavon7 <0999435@gmail.com>
2024-07-01 12:48:15 +0300

    01.07

--- css/burger-menu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.querySelector('.header__toggle');
-    const menu = document.querySelector('.header__menu');
-
-    toggleButton.addEventListener('click', function() {
-        this.classList.toggle('active');
-        menu.classList.toggle('active');
-    });
-});
